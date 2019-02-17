a08a847877ea210752896cba34e04505e67d9a11
zhangjingtao <alex_zjt@163.com>
2019-02-17 18:16:12 +0800

    Updates

---
+++ precache-manifest.c38334d0383663a97e3cf8a8d102efaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beab09985ca310db7425b6fb80cab56",
+    "url": "/hlddz/build/static/media/ddz-logo.6beab099.jpg"
+  },
+  {
+    "revision": "43a31c9806c30e263b03",
+    "url": "/hlddz/build/static/js/runtime~main.43a31c98.js"
+  },
+  {
+    "revision": "e216b7d2ec1eee272aa3",
+    "url": "/hlddz/build/static/js/main.e216b7d2.chunk.js"
+  },
+  {
+    "revision": "8a95fe2d88d2292b61d3",
+    "url": "/hlddz/build/static/js/2.8a95fe2d.chunk.js"
+  },
+  {
+    "revision": "e216b7d2ec1eee272aa3",
+    "url": "/hlddz/build/static/css/main.25dfa2df.chunk.css"
+  },
+  {
+    "revision": "8a95fe2d88d2292b61d3",
+    "url": "/hlddz/build/static/css/2.75cee52e.chunk.css"
+  },
+  {
+    "revision": "a3903ab8dcec19d91d624153df61ad41",
+    "url": "/hlddz/build/index.html"
+  }
+];
