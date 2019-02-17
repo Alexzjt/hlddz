a1a87f4b1cd5fe6104605867bbc1d871986a27e6
zhangjingtao <alex_zjt@163.com>
2019-02-17 17:49:57 +0800

    http://alexzjt.github.io/hlddz/build/

---
+++ build/precache-manifest.c4648212a2f1474eec6af31748e17992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beab09985ca310db7425b6fb80cab56",
+    "url": "/hlddz/build/static/media/ddz-logo.6beab099.jpg"
+  },
+  {
+    "revision": "43a31c9806c30e263b03",
+    "url": "/hlddz/build/static/js/runtime~main.43a31c98.js"
+  },
+  {
+    "revision": "bd4e261fb37f3f34d818",
+    "url": "/hlddz/build/static/js/main.bd4e261f.chunk.js"
+  },
+  {
+    "revision": "8a95fe2d88d2292b61d3",
+    "url": "/hlddz/build/static/js/2.8a95fe2d.chunk.js"
+  },
+  {
+    "revision": "bd4e261fb37f3f34d818",
+    "url": "/hlddz/build/static/css/main.0ec3e73a.chunk.css"
+  },
+  {
+    "revision": "8a95fe2d88d2292b61d3",
+    "url": "/hlddz/build/static/css/2.75cee52e.chunk.css"
+  },
+  {
+    "revision": "4f1d07670888311de3837a616d77c89c",
+    "url": "/hlddz/build/index.html"
+  }
+];
